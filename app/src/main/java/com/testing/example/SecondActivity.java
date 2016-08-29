4cb7b389f34ce6879b28fdb8f10ab93ca4a075ca
Rudrik Patel <rudrik.patel@zymr.com>
2016-08-29 17:12:36 +0530

    Branch Check

--- app/src/main/java/com/testing/example/SecondActivity.java
+++ app/src/main/java/com/testing/example/SecondActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class SecondActivity extends AppCompatActivity {
 
+    // Second
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
